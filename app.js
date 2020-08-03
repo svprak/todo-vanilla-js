909aa3e6ae0b0cf45872c097efb695dbf33b7959
svp <svprak@gmail.com>
2020-08-03 16:40:54 +0700

    Add Class done works

--- app.js
+++ app.js
@@ -2,16 +2,48 @@ const ul = document.querySelector('.list');
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 
+const todoList = JSON.parse(localStorage.items);
 
 function addItem() {
-    // e.preventDefault();
-
-
+  // e.preventDefault();
+  if (input.value !== '') {
+    todoList.push({ item: input.value.trim(), done: false });
     ul.innerHTML += '<li>' + input.value + '</li>';
-    input.value = "";
+    input.value = '';
     input.focus();
-    // console.log(input.textContent)
-}
 
-button.addEventListener('onclick', addItem)
-console.log(button)
+    saveToLocalStorage(todoList);
+  }
+  // console.log(input.textContent)
+}
+function saveToLocalStorage(items) {
+  localStorage.setItem('items', JSON.stringify(items));
+}
+function loadFromLocalStorage(todos) {
+  for (let i of todos) {
+    ul.innerHTML += '<li>' + i.item + '</li>';
+  }
+}
+function removeItem() {
+  //TODO
+  //Remove item from the todo list
+}
+function toggleDone(e) {
+  //TODO
+  //Check list if it is done
+  console.log(e.target);
+
+  if (e.target.tagName === 'LI') {
+    this.classList.add('done');
+  }
+}
+loadFromLocalStorage(todoList);
+
+// for (let item of localStorage.items) {
+//   console.log(item);
+// }
+console.log(ul.childNodes);
+for (let i = 0; i < ul.childNodes.length; i++) {
+  ul.childNodes[i].addEventListener('click', toggleDone);
+}
+button.addEventListener('onclick', addItem);
